6345165c912a318c357acea84c87cbdbfbf2d74d
MishaHerscu <mishaherscu@gmail.com>
2016-06-23 14:18:49 -0400

    routing specs

--- spec/routing/games_spec.rb
+++ spec/routing/games_spec.rb
@@ -1,37 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe 'routes for users' do
-  it 'routes POST /sign-up to the users#signup action' do
-    expect(post('/sign-up')).to route_to('users#signup')
+RSpec.describe 'routes for games' do
+  it 'routes POST /new-game to the games#new action' do
+    expect(post('/new-game')).to route_to('games#new')
   end
 
-  it 'routes POST /sign-in to the users#signin action' do
-    expect(post('/sign-in')).to route_to('users#signin')
-  end
-
-  it 'routes DELETE /sign-out/:id to the users#signout action' do
-    expect(delete('/sign-out/1')).to route_to(
-      controller: 'users',
-      action: 'signout',
+  it 'routes DELETE /delete-game/:id to the games#destroy action' do
+    expect(delete('/delete-game/1')).to route_to(
+      controller: 'games',
+      action: 'destroy',
       id: '1'
     )
   end
 
-  it 'routes PATCH /change-password/:id to the users#changepw action' do
-    expect(patch('/change-password/1')).to route_to(
-      controller: 'users',
-      action: 'changepw',
+  it 'routes PATCH /update-game/:id to the games#update action' do
+    expect(patch('/update-game/1')).to route_to(
+      controller: 'games',
+      action: 'update',
       id: '1'
     )
   end
 
-  it 'routes GET /users to the users#index action' do
-    expect(get('/users')).to route_to('users#index')
+  it 'routes GET /games to the games#index action' do
+    expect(get('/games')).to route_to('games#index')
   end
 
-  it 'routes GET /users/:id to the users#show action' do
-    expect(get('/users/1')).to route_to(
-      controller: 'users',
+  it 'routes GET /games/:id to the games#show action' do
+    expect(get('/games/1')).to route_to(
+      controller: 'games',
       action: 'show',
       id: '1'
     )
--- spec/routing/teams_spec.rb
+++ spec/routing/teams_spec.rb
@@ -1,37 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe 'routes for users' do
-  it 'routes POST /sign-up to the users#signup action' do
-    expect(post('/sign-up')).to route_to('users#signup')
+RSpec.describe 'routes for teams' do
+  it 'routes POST /new-team to the teams#new action' do
+    expect(post('/new-team')).to route_to('teams#new')
   end
 
-  it 'routes POST /sign-in to the users#signin action' do
-    expect(post('/sign-in')).to route_to('users#signin')
-  end
-
-  it 'routes DELETE /sign-out/:id to the users#signout action' do
-    expect(delete('/sign-out/1')).to route_to(
-      controller: 'users',
-      action: 'signout',
+  it 'routes DELETE /delete-team/:id to the teams#destroy action' do
+    expect(delete('/delete-team/1')).to route_to(
+      controller: 'teams',
+      action: 'destroy',
       id: '1'
     )
   end
 
-  it 'routes PATCH /change-password/:id to the users#changepw action' do
+  it 'routes PATCH /update-team/:id to the teams#update action' do
     expect(patch('/change-password/1')).to route_to(
       controller: 'users',
-      action: 'changepw',
+      action: 'update',
       id: '1'
     )
   end
 
-  it 'routes GET /users to the users#index action' do
-    expect(get('/users')).to route_to('users#index')
+  it 'routes GET /teams to the teams#index action' do
+    expect(get('/teams')).to route_to('teams#index')
   end
 
-  it 'routes GET /users/:id to the users#show action' do
-    expect(get('/users/1')).to route_to(
-      controller: 'users',
+  it 'routes GET /teams/:id to the teams#show action' do
+    expect(get('/teams/1')).to route_to(
+      controller: 'teams',
       action: 'show',
       id: '1'
     )
