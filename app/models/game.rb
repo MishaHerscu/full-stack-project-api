2dfc2f952fc3ec66d0c735789bae2d5937a68865
MishaHerscu <mishaherscu@gmail.com>
2016-06-24 01:01:52 -0400

    added Authentication mixin to models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,5 @@
 #
 class Game < ActiveRecord::Base
+  include Authentication
   belongs_to :team, inverse_of: :games
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,6 @@
 #
 class Player < ActiveRecord::Base
+  include Authentication
   belongs_to :team, inverse_of: :players
   belongs_to :user, inverse_of: :player
 end
