0750e76c439aea53d43d59c6706ca3783decb822
MishaHerscu <mishaherscu@gmail.com>
2016-06-28 00:07:06 -0400

    added goals, assists to players, games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 #
 class Game < ActiveRecord::Base
   belongs_to :team, inverse_of: :games, dependent: :destroy
+  has_many :goals, inverse_of: :player, dependent: :destroy
+  has_many :assists, inverse_of: :player, dependent: :destroy
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,6 @@
 class Player < ActiveRecord::Base
   belongs_to :team, inverse_of: :players, dependent: :destroy
   belongs_to :user, inverse_of: :player, dependent: :destroy
+  has_many :goals, inverse_of: :player, dependent: :destroy
+  has_many :assists, inverse_of: :player, dependent: :destroy
 end
