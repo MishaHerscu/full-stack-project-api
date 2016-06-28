88d2f067ccef71dc1e4530eee3ae620c36524098
MishaHerscu <mishaherscu@gmail.com>
2016-06-28 00:12:53 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623165540) do
+ActiveRecord::Schema.define(version: 20160628035834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "assists", force: :cascade do |t|
+    t.integer  "player_id"
+    t.integer  "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "assists", ["game_id"], name: "index_assists_on_game_id", using: :btree
+  add_index "assists", ["player_id"], name: "index_assists_on_player_id", using: :btree
 
   create_table "examples", force: :cascade do |t|
     t.text     "text",       null: false
@@ -35,6 +45,16 @@ ActiveRecord::Schema.define(version: 20160623165540) do
   end
 
   add_index "games", ["team_id"], name: "index_games_on_team_id", using: :btree
+
+  create_table "goals", force: :cascade do |t|
+    t.integer  "player_id"
+    t.integer  "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "goals", ["game_id"], name: "index_goals_on_game_id", using: :btree
+  add_index "goals", ["player_id"], name: "index_goals_on_player_id", using: :btree
 
   create_table "players", force: :cascade do |t|
     t.string   "surname"
@@ -68,8 +88,12 @@ ActiveRecord::Schema.define(version: 20160623165540) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["token"], name: "index_users_on_token", unique: true, using: :btree
 
+  add_foreign_key "assists", "games"
+  add_foreign_key "assists", "players"
   add_foreign_key "examples", "users"
   add_foreign_key "games", "teams"
+  add_foreign_key "goals", "games"
+  add_foreign_key "goals", "players"
   add_foreign_key "players", "teams"
   add_foreign_key "players", "users"
 end
